d146a9a1283dcaba5ed291a98a3a129e354e34db
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2020-08-08 23:56:08 +0000

    Revert "Merge branch 'modules/03-conquistando-o-seu-cliente' into 'master'"
    This reverts merge request !47

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -5,14 +5,13 @@ from eventex.subscriptions.models import Subscription
 
 
 class SubscriptionModelAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'phone',
-                    'cpf', 'created_at', 'subscribed_today',
-                    'paid')
+    list_display = ('name', 'email', 'phone', 'cpf', 'created_at', 'subscribed_today')
     date_hierarchy = 'created_at'
     search_fields = ('name', 'email', 'phone', 'cpf', 'created_at')
-    list_filter = ('paid', 'created_at')
+    list_filter = ('created_at',)
 
     def subscribed_today(self, obj):
+        print('teste')
         return obj.created_at.date() == now().date()
 
     subscribed_today.short_description = 'inscrito hoje?'
