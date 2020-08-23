65fe831b5c1198c15d5573d09f55aacc8943290a
Rodrigo Rocha <rodrigo.srrocha@gmail.com>
2020-08-23 15:56:05 -0300

    Ajustes de lint e target blank

--- eventex/core/admin.py
+++ eventex/core/admin.py
@@ -5,23 +5,23 @@ from eventex.core.models import Speaker, Contact
 
 
 class ContactInline(admin.TabularInline):
-  model = Contact
-  extra = 1
+    model = Contact
+    extra = 1
 
 
 class SpeakerModelAdmin(admin.ModelAdmin):
-  inlines = [ContactInline]
-  prepopulated_fields = {'slug': ('name',)}
-  list_display = ['name', 'photo_img', 'website_link']
+    inlines = [ContactInline]
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ['name', 'photo_img', 'website_link']
 
-  def website_link(self, obj):
-    return format_html('<a href="{0}">{0}</a>', obj.website)
+    def website_link(self, obj):
+        return format_html('<a href="{0}">{0}</a>', obj.website)
 
-  website_link.short_description = 'website'
+    website_link.short_description = 'website'
 
-  def photo_img(self, obj):
-    return format_html('<img width="32px" src="{}"/>', obj.photo)
+    def photo_img(self, obj):
+        return format_html('<img width="32px" src="{}"/>', obj.photo)
 
-  photo_img.short_description = 'foto'
+    photo_img.short_description = 'foto'
 
 admin.site.register(Speaker, SpeakerModelAdmin)
--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from eventex.core.models import Speaker 
+from eventex.core.models import Speaker
 
 def home(request):
     speakers = Speaker.objects.all()
@@ -8,4 +8,4 @@ def home(request):
 
 def speaker_detail(request, slug):
     speaker = get_object_or_404(Speaker, slug=slug)
-    return render(request, 'core/speaker_detail.html', { 'speaker': speaker})
+    return render(request, 'core/speaker_detail.html', {'speaker': speaker})
